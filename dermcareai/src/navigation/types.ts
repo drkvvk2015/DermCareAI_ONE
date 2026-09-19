b05094e4d4a3e05fbfb04e845d4d0ae14586ae56
VIJAYAKUMAR SUBRAMANIYAM <75628027+drkvvk2015@users.noreply.github.com>
2026-09-19 12:46:42 +0530

    refactor(mobile): use shared governance types

--- dermcareai/src/navigation/types.ts
+++ dermcareai/src/navigation/types.ts
@@ -15,19 +15,7 @@ export type Patient = {
   upcomingVisit?: string;
 };
 
-export interface AIGovernanceCard {
-  decision_type: 'clinical_decision_support';
-  intended_use: string;
-  diagnostic_status: 'not_a_diagnosis';
-  human_review_required: boolean;
-  abstention_enabled: boolean;
-  confidence_threshold: number;
-  model_provenance: string;
-  model_name: string;
-  research_model: boolean;
-  safety_controls: string[];
-  limitations: string[];
-}
+import { AIGovernanceCard } from '../types/platform';
 
 export interface ScreeningReport {
   id: string;
